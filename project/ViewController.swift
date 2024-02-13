d79f48996d57dc2bb0a9cb8a664a914770291ce6
@fedotovaa <fedotova_a0@mail.ru>
2024-02-13 20:26:08 +0300

    Initial Commit

---
+++ project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project
+//
+//  Created by Anna Fedotova on 13.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
