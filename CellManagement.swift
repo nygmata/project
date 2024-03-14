d54f9d5a7d0a3dff5d1e39f1ca7c542304511e8b
nygmata <66067912+nygmata@users.noreply.github.com>
2024-03-14 12:38:38 +0300

    Update CellManagement.swift

--- CellManagement.swift
+++ CellManagement.swift
@@ -5,7 +5,7 @@ let times = ["6AM", "7AM", "8AM", "9AM", "10AM", "11AM", "12AM", "1PM", "2PM", "
 
 let timeToIndex: [String: Int] = Dictionary(uniqueKeysWithValues: zip(times, 0...17))
 
-class CellManagement: UIViewController {
+class CellManagement {
     
     func createCells(_ cells: inout [Cell]) {
         for time in times {
@@ -57,5 +57,47 @@ class CellManagement: UIViewController {
         } catch let error {
             print("Error saving cell data: \(error.localizedDescription)")
         }
-    }    
+    }
+    
+    public func loadTextData<T: NSManagedObject>(textView: UITextView, _ entity: T.Type) {
+
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+
+        let fetchRequest: NSFetchRequest<T> = NSFetchRequest<T>(entityName: String(describing: T.self))
+
+        do {
+            let textEntities = try context.fetch(fetchRequest)
+            var newText = ""
+            for textEntity in textEntities {
+                guard let text = textEntity.value(forKey: "text") as? String else {
+                    throw NSError(domain: "com.example", code: 1, userInfo: [NSLocalizedDescriptionKey: "Failed to fetch entity properties"])
+                }
+                newText = text
+            }
+            
+            textView.text = newText
+        } catch let error {
+            print("Error fetching cell data: \(error.localizedDescription)")
+        }
+    }
+    
+    func saveTextData<T: NSManagedObject>(text: String, _ entity: T.Type) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+
+        guard let entityDescription = NSEntityDescription.entity(forEntityName: String(describing: T.self), in: context) else {
+            fatalError("Entity description not found for \(T.self)")
+        }
+
+        let cellEntity = T(entity: entityDescription, insertInto: context)
+        cellEntity.setValue(text, forKey: "text")
+
+        do {
+            try context.save()
+            print("Text data saved successfully")
+        } catch let error {
+            print("Error saving text data: \(error.localizedDescription)")
+        }
+    }
 }
